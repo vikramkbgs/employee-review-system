c12d6fd63dfbb1b915ac2c653351e3f5426c8603
vikramkbgs <vikramkbgs@gmail.com>
2024-04-10 09:04:14 +0530

    updating config

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 
+// for environment variables
 require('dotenv').config();
+
+// connect to database
 require('./config/mongoose').connect();
+
+// express
 const express = require('express');
 
-const PORT = process.env.PORT;
+// passport local strategy
+const passportConfig = require('./config/passport_local');
+// passport
+const passport = require('passport');
 
-const app = express();
+// for parsing the data in cookie
+const cookieParser = require('cookie-parser')
 
-app.use(express.json());
-app.use(express.urlencoded({
-    extended:true
-})); 
-app.use(express.static('assets'));
+// store the session create by passport
+const session=require('express-session');
 
 // importing layouts 
 const expressLayouts =  require('express-ejs-layouts');
 
-// using layouts
+// flash messages package and middleware
+const flash = require('connect-flash');
+const myMware=require('./config/middleware');
 
+// store the session in mongostore
+const MongoStore = require('connect-mongo');
+
+// port
+const PORT = process.env.PORT;
+
+// creatin app
+const app = express();
+
+// middlewares
+
+// for reading json data
+app.use(express.json());
+// for reading url data
+app.use(express.urlencoded({
+    extended:true
+})); 
+
+// for static files folder
+app.use(express.static('assets'));
+
+// for parsing the cookies
+app.use(cookieParser());
+
+// using layouts
 app.use(expressLayouts);
 
 // extracting stylesheets and scripts for individual pages
@@ -29,6 +62,35 @@ app.set('layout extractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 
+// Use express-session for session management
+app.use(session({
+    secret: process.env.SECRET_KEY, // Replace with your own secret key
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // Session duration in milliseconds (1 day in this example)
+      secure: false, // Set this to false to allow the session over HTTP
+    },
+    // store the session in database
+    store: MongoStore.create({
+      mongoUrl:process.env.MONGODB_URL
+    })
+  }));
+  
+// connect-flash middleware
+app.use(flash());
+app.use(myMware.setFlash);
+
+// initialize passport
+app.use(passport.initialize());
+// passport sessions
+app.use(passport.session());
+
+// store the logged in user's data in locals variable
+app.use(passport.setAuthenticatedUser);
+
+// routes
 app.use('/',require('./routes'));
 
+// fire up server
 app.listen(PORT,() => console.log(`Server is running on port: ${PORT}`));
